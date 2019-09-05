f3c92c4dabd339614de617ac4a7908ed8ccf7d22
apolsito <34385814+apolsito@users.noreply.github.com>
2019-09-05 13:18:43 -0500

    avance

--- SparseMatrix.cpp
+++ SparseMatrix.cpp
@@ -5,21 +5,6 @@
 #include "SparseMatrix.h"
 #include <iostream>
 
-/*
-
- void multiplicarMatriz(int **pM1,int **pM2,int **pM3,unsigned int f1,unsigned int c1,unsigned int f2,unsigned int c2,unsigned int f3,unsigned int c3){
-
-    for (int i = 0; i < f3; i++){
-        for (int j = 0; j < c3; j++){
-            pM3[i][j] = 0;
-            for(int k = 0; k < f3; k++){
-                pM3[i][j]+= pM1[i][k] * pM2[k][j];
-            }
-        }
-    }
-}
-*/
-
 SparseMatrix::SparseMatrix() {
     matriz = nullptr;
     filas = 0;
@@ -95,7 +80,11 @@ SparseMatrix SparseMatrix::operator+(const SparseMatrix &otro) {
         rangoMa = otro.sizeSM;
         rangoMe = sizeSM;
 
+
     }
+
+    cout << rangoMa << "\n";
+    cout << rangoMe << "\n";
     SparseMatrix temporal3(3,rangoMa);
     temporal3.pedirEspacio();
 
@@ -135,80 +124,73 @@ SparseMatrix SparseMatrix::operator+(const SparseMatrix &otro) {
         }
         cout <<"\n";
     }
-/*
-int n = 0;
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < otro.columnas; j++)
-            while (temporal1.matriz[i][j] < temporal2.matriz[i][j])
-           // temp
-            {
-                temporal3.matriz[0][m] = i;
-                temporal3.matriz[1][m] = j;
-                temporal3.matriz[2][m] = otro.matriz[i][j];
-                n++;
-            }
-*/
 
+int n = 0, p = 0;
+    for (int i = 0; i < rangoMa; i++){
+        if(temporal1.matriz[0][p]<temporal2.matriz[0][n]){
+            temporal3.matriz[2][i] = temporal1.matriz[2][p];
 
+            if(p<sizeSM)
 
-int n= 0;
-    for(int i = 0; i < rangoMa; i++){
-        if (temporal1.matriz[0][i] < temporal2.matriz[0][n] || n == rangoMe){
-            temporal3.matriz[0][i] = temporal1.matriz[0][i];
-            temporal3.matriz[1][i] = temporal1.matriz[1][i];
-            temporal3.matriz[2][i] = temporal1.matriz[2][i];
+                p++;
         }
-
-
-
-        else if (temporal1.matriz[0][i] == temporal2.matriz[0][n]){
-                if(temporal1.matriz[1][i] < temporal2.matriz[1][n]){
-                    temporal3.matriz[0][i] = temporal1.matriz[0][i];
-                    temporal3.matriz[1][i] = temporal1.matriz[1][i];
-                    temporal3.matriz[2][i] = temporal1.matriz[2][i];
-                }
-                    else if ((temporal1.matriz[1][i] == temporal2.matriz[1][n])){
-                    temporal3.matriz[0][i] = temporal1.matriz[0][i];
-                    temporal3.matriz[1][i] = temporal1.matriz[1][i];
-                    temporal3.matriz[2][i] = temporal1.matriz[2][i]+temporal2.matriz[2][n];
-                    if(n<rangoMe)
-                        n++;
-                }
-                    else{
-                    temporal3.matriz[0][i] = temporal2.matriz[0][n];
-                    temporal3.matriz[1][i] = temporal2.matriz[1][n];
-                    temporal3.matriz[2][i] = temporal2.matriz[2][n];
-                    if(n<rangoMe)
-                        n++;
-                }
-                    }
-
-        else{
-
-            temporal3.matriz[0][i] = temporal2.matriz[0][n];
-            temporal3.matriz[1][i] = temporal2.matriz[1][n];
+        else if(temporal1.matriz[0][p]==temporal2.matriz[0][n]){
+            if(temporal1.matriz[1][p]<temporal2.matriz[1][n]){
+                temporal3.matriz[2][i] = temporal1.matriz[2][p];
+                if(p<sizeSM)
+                    p++;
+            }
+            else if(temporal1.matriz[1][p]==temporal2.matriz[1][n]){
+                temporal3.matriz[2][i] = temporal1.matriz[2][p]+temporal2.matriz[2][n];
+                if(n<otro.sizeSM)
+                    n++;
+                if(p<sizeSM)
+                    p++;
+            }
+            else if(temporal1.matriz[1][p]>temporal2.matriz[1][n]){
+                temporal3.matriz[2][i] = temporal2.matriz[2][n];
+                if(n<otro.sizeSM)
+                    n++;
+            }
+        }
+        else if(temporal1.matriz[0][p]>temporal2.matriz[0][n]){
             temporal3.matriz[2][i] = temporal2.matriz[2][n];
-            if(n<rangoMe)
+            if(n<otro.sizeSM)
                 n++;
-        }}
-
+        }
+    }
 
 
     for (int i=0; i<rangoMa; i++){
-        for (int j=0; j<rangoMa; j++){
-            cout << temporal3.matriz[i][j] << " ";
-        }
+        cout << temporal3.matriz[2][i]<<" ";
 
-        cout <<"\n";
-            }
-
+    }
 }
 
 SparseMatrix SparseMatrix::operator *(const SparseMatrix &otro) {
+    unsigned int rangoMa = 0, rangoMe = 0;
     SparseMatrix temporal1(3,sizeSM);
     SparseMatrix temporal2(3,otro.sizeSM);
     temporal1.pedirEspacio();
     temporal2.pedirEspacio();
+
+    if(sizeSM > otro.sizeSM){
+        rangoMa = sizeSM;
+        rangoMe = otro.sizeSM;
+
+    }
+    else{
+
+        rangoMa = otro.sizeSM;
+        rangoMe = sizeSM;
+
+
+    }
+
+    cout << rangoMa << "\n";
+    cout << rangoMe << "\n";
+    SparseMatrix temporal3(3,rangoMa);
+    temporal3.pedirEspacio();
 
     int k = 0;
     for (int i = 0; i < filas; i++)
@@ -220,6 +202,8 @@ SparseMatrix SparseMatrix::operator *(const SparseMatrix &otro) {
                 temporal1.matriz[2][k] = matriz[i][j];
                 k++;
             }
+
+
     for (int i=0; i<3; i++){
         for (int j=0; j<sizeSM; j++){
             cout << temporal1.matriz[i][j] << " ";
@@ -227,7 +211,21 @@ SparseMatrix SparseMatrix::operator *(const SparseMatrix &otro) {
         cout <<"\n";
     }
 
-    for (int i = 0; i < temporal1.filas; i++)
-      for (int j = 0; j < sizeSM; j++)
-        temporal1.matriz[i] = temporal1.matriz[i] + temporal1.matriz[2][i] *temporal1.matriz[1][j] ;
+
+    int m = 0;
+    for (int i = 0; i < otro.filas; i++)
+        for (int j = 0; j < otro.columnas; j++)
+            if (otro.matriz[i][j] != 0)
+            {
+                temporal2.matriz[0][m] = i;
+                temporal2.matriz[1][m] = j;
+                temporal2.matriz[2][m] = otro.matriz[i][j];
+                m++;
+            }
+    for (int i=0; i<3; i++){
+        for (int j=0; j<otro.sizeSM; j++){
+            cout << temporal2.matriz[i][j] << " ";
+        }
+        cout <<"\n";
+    }
 }
